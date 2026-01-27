d5110cde235d2bd8546229a98a5def2632b2980c
Muse.Morgan <musemorgan@yahoo.com>
2026-01-27 12:32:25 -0500

    Completed displayGrades.c (writes output from file)

--- Warmup Practice/Grade Reader/displayGrades.c
+++ Warmup Practice/Grade Reader/displayGrades.c
@@ -15,11 +15,28 @@ int main(){
         return 1;
     }
 
-    printf("DEBUG: Successfully opened the file!\n")
+    printf("DEBUG: Successfully opened the file!\n");
 
+    while(fscanf(file, "%d", &grade) == 1)
+    {
+        printf("DEBUG: grade #%d\n", grade);
+        sum = sum + grade;
+        count = count + 1;
+    }
 
+    printf("DEBUG: Finished reading, total number of grades = %d\n", count);
+    fclose(file);
 
-
+    if (count > 0 ){
+        average = sum /(float)count; //casting to another data type can be done with parenthesis and the data type inside
+        printf("===============RESULTS=============\n");
+        printf("Number of grades: %d\n", count);
+        printf("Sum: %d\n", sum);
+        printf("Average: %.2f\n", average);
+    }
+    else{
+        printf("DEBUG: No grades found.");
+    }
 
     return 0;
 }
