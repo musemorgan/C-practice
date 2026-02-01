ae022a0c8679986fffe9f98ec8c57e451e6e17dd
Muse.Morgan <musemorgan@yahoo.com>
2026-02-01 00:59:51 -0500

    Initial strcut learning project

--- Claude Warmup Practice/Structs/StudentStruct.c
+++ Claude Warmup Practice/Structs/StudentStruct.c
@@ -15,11 +15,13 @@ int main(){
 //struct Student s1 = {"Alice, 12345, 95"};
 
 struct Student s1;
-strcpy(s1.name, "Alice");
+strcpy(s1.name, "Alice"); //Why is this formatted this way, but not for the other two items below? Strings can be assigned directly in c
 s1.id = 12345;
 s1.score = 95;
+struct Student s2 = {"Morgan", 10234, 99};
 
 printf("Student: %s (ID: %d) - Score: %d\n", s1.name, s1.id, s1.score);
+printf("student: %s (ID: %d) - Score: %d\n", s2.name, s2.id, s2.score);
 
     return 0;
 }
